e17fba32fdb52d98934278d30081e7a909ec2f78
Ernest <asieduernest@yahoo.co.uk>
2022-07-06 04:41:09 -0400

    Update(dev): run eslint fix

--- src/inject/lessons.js
+++ src/inject/lessons.js
@@ -1,21 +1,21 @@
-import { AlertError } from "./rate_finder"
+import { AlertError } from './rate_finder';
 
-export default function Lessons(/**@type Element*/ container_element, /**@type string */ url) {
-	let lesson_map
+export default function Lessons(/** @type Element */ container_element, /** @type string */ url) {
+  let lesson_map;
 
-	function renderEarningStats() {
-		if (!url.includes("https://www.wyzant.com/tutor/lessons")) throw new AlertError("Error: invalid page url")
+  function renderEarningStats() {
+    if (!url.includes('https://www.wyzant.com/tutor/lessons')) throw new AlertError('Error: invalid page url');
 
-		let lessons = [...document.querySelectorAll('[id="LessonsList"] tbody tr').values()].map(makeLesson)
+    const lessons = [...document.querySelectorAll('[id="LessonsList"] tbody tr').values()].map(makeLesson);
 
-		let stat_values = [...getDateStats(lessons).values()]
+    const stat_values = [...getDateStats(lessons).values()];
 
-		let avg_earned = parseFloat(stat_values.map(({ earned }) => earned).reduce((acc, earned) => acc + earned) / stat_values.length + "").toFixed(2)
-		let avg_tutored = parseFloat(stat_values.map(({ tutored_minutes }) => tutored_minutes).reduce((acc, tutored_minutes) => acc + tutored_minutes) / stat_values.length + "").toFixed(2)
+    const avg_earned = parseFloat(`${stat_values.map(({ earned }) => earned).reduce((acc, earned) => acc + earned) / stat_values.length}`).toFixed(2);
+    const avg_tutored = parseFloat(`${stat_values.map(({ tutored_minutes }) => tutored_minutes).reduce((acc, tutored_minutes) => acc + tutored_minutes) / stat_values.length}`).toFixed(2);
 
-		let stats_row_html = stat_values.map(makeDateStatElement).join("")
+    const stats_row_html = stat_values.map(makeDateStatElement).join('');
 
-		let stats_table_html = `
+    const stats_table_html = `
 		<div class="wzl-card bg-hue">
 			<table class="wzl_date_stats_table ">
 				<thead>
@@ -29,67 +29,71 @@ export default function Lessons(/**@type Element*/ container_element, /**@type s
 				</tbody>
 			</table>
 		</div
-		`
+		`;
 
-		container_element.innerHTML = stats_table_html
-	}
+    container_element.innerHTML = stats_table_html;
+  }
 
-	function makeDateStatElement(/**@type Lesson*/ date_stat) {
-		return `
+  function makeDateStatElement(/** @type Lesson */ date_stat) {
+    return `
 			<tr>
 				<td>${date_stat.date}</td>
 				<td>$${date_stat.earned}</td>
 				<td>${date_stat.tutored_minutes} mins</td>
 			</tr>
-		`
-	}
+		`;
+  }
 
-	/**@return { Map<string,LessonEntry>} */
-	function getDateStats(lessons) {
-		let /**@type Map<string,LessonEntry>*/ daily_stats = new Map()
+  /** @return { Map<string,LessonEntry>} */
+  function getDateStats(lessons) {
+    const /** @type Map<string,LessonEntry> */ daily_stats = new Map();
 
-		for (let day of lessons) {
-			let /**@type {DateStat} */ date_stat = daily_stats.get(day.date.replace("*", "")) ?? makeDateStat(day)
+    for (const day of lessons) {
+      const /** @type {DateStat} */ date_stat = daily_stats.get(day.date.replace('*', '')) ?? makeDateStat(day);
 
-			date_stat.earned += parseFloat(day.earned.replace("$", "").trim())
-			date_stat.tutored_minutes += parseFloat(day.length.replace("min", "").trim())
+      date_stat.earned += parseFloat(day.earned.replace('$', '').trim());
+      date_stat.tutored_minutes += parseFloat(day.length.replace('min', '').trim());
 
-			daily_stats.set(date_stat.date, date_stat)
-		}
+      daily_stats.set(date_stat.date, date_stat);
+    }
 
-		return daily_stats
-	}
+    return daily_stats;
+  }
 
-	/**@alias DateStat */
-	function makeDateStat(day) {
-		/**@lends */
-		return { earned: 0, tutored_minutes: 0, payed: day.date.includes("*"), date: day.date.replace("*", "") }
-	}
+  /** @alias DateStat */
+  function makeDateStat(day) {
+    /** @lends */
+    return {
+      earned: 0, tutored_minutes: 0, payed: day.date.includes('*'), date: day.date.replace('*', ''),
+    };
+  }
 
-	function renderLessonStats(/**@type [] */ parsed_lesson) {}
+  function renderLessonStats(/** @type [] */ parsed_lesson) {}
 
-	/**@alias Lesson */
-	function makeLesson(/**@type Element*/ lesson_row_el) {
-		const PROPS = { Date: 0, Length: 1, Entered: 2, Online: 3, Student: 4, Subject: 5, Rating: 6, Rate: 7, Pay: 8, Earned: 9, Mileage: 10, Payment: 11, Status: 12 }
+  /** @alias Lesson */
+  function makeLesson(/** @type Element */ lesson_row_el) {
+    const PROPS = {
+      Date: 0, Length: 1, Entered: 2, Online: 3, Student: 4, Subject: 5, Rating: 6, Rate: 7, Pay: 8, Earned: 9, Mileage: 10, Payment: 11, Status: 12,
+    };
 
-		/**@lends */
-		return {
-			date: grabValue(lesson_row_el, PROPS.Date).match(/\d+\/\d+\/\d+/)[0],
-			student: grabValue(lesson_row_el, PROPS.Student),
-			length: grabValue(lesson_row_el, PROPS.Length).match(/\d+/)[0],
-			entered: grabValue(lesson_row_el, PROPS.Entered).match(/\d+\/\d+\/\d+/)[0],
-			earned: grabValue(lesson_row_el, PROPS.Earned).match(/\d+(.\d+)?/)[0],
-			rate: (grabValue(lesson_row_el, PROPS.Rate)?.match(/\d+(.\d+)?/) ?? ["--"])[0],
-		}
-	}
+    /** @lends */
+    return {
+      date: grabValue(lesson_row_el, PROPS.Date).match(/\d+\/\d+\/\d+/)[0],
+      student: grabValue(lesson_row_el, PROPS.Student),
+      length: grabValue(lesson_row_el, PROPS.Length).match(/\d+/)[0],
+      entered: grabValue(lesson_row_el, PROPS.Entered).match(/\d+\/\d+\/\d+/)[0],
+      earned: grabValue(lesson_row_el, PROPS.Earned).match(/\d+(.\d+)?/)[0],
+      rate: (grabValue(lesson_row_el, PROPS.Rate)?.match(/\d+(.\d+)?/) ?? ['--'])[0],
+    };
+  }
 
-	function grabValue(/**@type Element*/ element, value_index) {
-		return element
-			.querySelectorAll("td")
-			[value_index].textContent.trim()
-			.replace(/[\t\n]*/gi, "")
-			.replace(/[ ]+/, " ")
-	}
+  function grabValue(/** @type Element */ element, value_index) {
+    return element
+      .querySelectorAll('td')
+      [value_index].textContent.trim()
+      .replace(/[\t\n]*/gi, '')
+      .replace(/[ ]+/, ' ');
+  }
 
-	renderEarningStats()
+  renderEarningStats();
 }
--- src/inject/setupJobApplicationPage.js
+++ src/inject/setupJobApplicationPage.js
@@ -1,72 +1,76 @@
-import { AlertError } from "./rate_finder"
+import { AlertError } from './rate_finder';
 
 export function setupJobApplicationPage() {
-	let /**@type HTMLInputElement*/ hourly_rate_input = document.querySelector("input[name=hourly_rate]")
+  const /** @type HTMLInputElement */ hourly_rate_input = document.querySelector('input[name=hourly_rate]');
 
-	if (!hourly_rate_input) throw new AlertError("hourly_rate_input is null")
+  if (!hourly_rate_input) throw new AlertError('hourly_rate_input is null');
 
-	hourly_rate_input.setAttribute("type", "number")
-	hourly_rate_input.setAttribute("step", "5")
+  hourly_rate_input.setAttribute('type', 'number');
+  hourly_rate_input.setAttribute('step', '5');
 
-	adjustPriceUpOnJobApplicationPage(document.querySelector(".hourly-rate-label + p"), hourly_rate_input)
+  adjustPriceUpOnJobApplicationPage(document.querySelector('.hourly-rate-label + p'), hourly_rate_input);
 
-	selectDefaultApplicationMessageResponse()
+  selectDefaultApplicationMessageResponse();
 
-	focusSubmitButton()
+  focusSubmitButton();
 
-	makeJobDetailsCardPositionSticky()
+  makeJobDetailsCardPositionSticky();
 }
 
 function focusSubmitButton() {
-	let /**@type HTMLElement */ button = document.querySelector("input[type=submit]")
+  const /** @type HTMLElement */ button = document.querySelector('input[type=submit]');
 
-	if (!button) throw new AlertError("input[type=submit] not found")
+  if (!button) throw new AlertError('input[type=submit] not found');
 
-	button.focus()
+  button.focus();
 }
 
-function adjustPriceUpOnJobApplicationPage(/**@type HTMLElement*/ hour_rate_label_node, /**@type HTMLInputElement*/ hourly_rate_input_node) {
-	if (!hourly_rate_input_node || !hour_rate_label_node) return
+function adjustPriceUpOnJobApplicationPage(/** @type HTMLElement */ hour_rate_label_node, /** @type HTMLInputElement */ hourly_rate_input_node) {
+  if (!hourly_rate_input_node || !hour_rate_label_node) return;
 
-	let [hour_rate_label_value] = hour_rate_label_node.textContent.match(/\d*$/)
+  const [hour_rate_label_value] = hour_rate_label_node.textContent.match(/\d*$/);
 
-	if (!isJobOfferHigher(hour_rate_label_value, hourly_rate_input_node)) {
-		return
-	}
+  if (!isJobOfferHigher(hour_rate_label_value, hourly_rate_input_node)) {
+    return;
+  }
 
-	console.log("high offer present")
+  console.log('high offer present');
 
-	if (Number(hour_rate_label_value) <= 70) {
-		console.log("charge exact offer")
-		hourly_rate_input_node.value = "" + Number(hour_rate_label_value)
-	} else {
-		console.log("charging base plus difference")
-		let difference = 0.6 * (Number(hour_rate_label_value) - 70)
-		hourly_rate_input_node.value = "" + Math.ceil((70 + difference) / 5) * 5
-	}
+  if (Number(hour_rate_label_value) <= 70) {
+    console.log('charge exact offer');
+    hourly_rate_input_node.value = `${Number(hour_rate_label_value)}`;
+
+    if (Number(hour_rate_label_value) % 5 !== 0) {
+      hourly_rate_input_node.removeAttribute('step');
+    }
+  } else {
+    console.log('charging base plus difference');
+    const difference = 0.6 * (Number(hour_rate_label_value) - 70);
+    hourly_rate_input_node.value = `${Math.ceil((70 + difference) / 5) * 5}`;
+  }
 }
 
 function isJobOfferHigher(hour_rate_label_value, hourly_rate_input_node) {
-	return Number(hour_rate_label_value) > parseInt(hourly_rate_input_node.value)
+  return Number(hour_rate_label_value) > parseInt(hourly_rate_input_node.value);
 }
 
 function selectDefaultApplicationMessageResponse() {
-	let default_response_option = Array.from(document.querySelectorAll("select[name=template_select] > option").values()).find(({ textContent }) => textContent === "default application")
+  const default_response_option = Array.from(document.querySelectorAll('select[name=template_select] > option').values()).find(({ textContent }) => textContent === 'default application');
 
-	if (!default_response_option) return
+  if (!default_response_option) return;
 
-	// @ts-ignore
-	document.querySelector("select[name=template_select]").value = default_response_option.value
+  // @ts-ignore
+  document.querySelector('select[name=template_select]').value = default_response_option.value;
 
-	// @ts-ignore
-	document.querySelector("textarea#personal_message").value = default_response_option.value
+  // @ts-ignore
+  document.querySelector('textarea#personal_message').value = default_response_option.value;
 }
 
 function makeJobDetailsCardPositionSticky() {
-	let /**@type HTMLElement */ job_details_card = document.querySelector(".columns.medium-4")
+  const /** @type HTMLElement */ job_details_card = document.querySelector('.columns.medium-4');
 
-	if (!job_details_card) throw new AlertError("Error: job_details_card not found")
+  if (!job_details_card) throw new AlertError('Error: job_details_card not found');
 
-	job_details_card.style.position = "sticky"
-	job_details_card.style.top = "10px"
+  job_details_card.style.position = 'sticky';
+  job_details_card.style.top = '10px';
 }
